709d6c24f4153fe72d5d59feb153815889b4084a
carlos <l20140951@queretaro.tecnm.mx>
2022-09-04 17:38:26 -0500

    Prueba

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Evalua
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lenguaje a = new Lenguaje();
+
+                a.Programa();
+                
+                //while(!a.FinArchivo())
+                //{
+                  //  a.NextToken();
+                //}
+                a.cerrar();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+    }
+}
